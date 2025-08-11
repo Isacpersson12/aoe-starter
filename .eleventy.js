d7e78accef164a6ff66ea8c591946a09f3e43581
Isacpersson12 <ip.isacpersson@gmail.com>
2025-08-11 16:27:32 +0200

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ export default function(eleventyConfig) {
   return {
     dir: { input: "src", output: "_site" },
     markdownTemplateEngine: "njk",
-    htmlTemplateEngine: "njk"
+    htmlTemplateEngine: "njk",
+    pathPrefix: "/aoe-starter/"   // <-- for GitHub Pages project site
   };
 }
