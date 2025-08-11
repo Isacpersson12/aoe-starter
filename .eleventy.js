b3d69a8f012faf24461f5510821135da40a3cd64
Isacpersson12 <ip.isacpersson@gmail.com>
2025-08-11 13:24:28 +0200

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,12 @@
 export default function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy({"public": "/"});
-  eleventyConfig.addCollection("opps", function(collectionApi) {
-    return collectionApi.getFilteredByGlob("src/opps/*.md").sort((a,b)=>{
+  eleventyConfig.addCollection("opps", (collectionApi) =>
+    collectionApi.getFilteredByGlob("src/opps/*.md").sort((a,b)=>{
       const da = (a.data.deadline||"").toString();
       const db = (b.data.deadline||"").toString();
       return da.localeCompare(db);
-    });
-  });
+    })
+  );
   return {
     dir: { input: "src", output: "_site" },
     markdownTemplateEngine: "njk",
