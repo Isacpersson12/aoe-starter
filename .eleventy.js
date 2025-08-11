2313b70f2808cc96372e2c7ae6ee067deba0189d
Isacpersson12 <ip.isacpersson@gmail.com>
2025-08-11 16:02:54 +0200

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,4 @@
 export default function(eleventyConfig) {
-  // Collect ANY markdown that lives under src/opps (robust on CI paths)
-  eleventyConfig.addCollection("opps", (api) => {
-    return api.getAll().filter(i =>
-      i.inputPath &&
-      i.inputPath.endsWith(".md") &&
-      (i.inputPath.includes("/src/opps/") || i.inputPath.includes("\\src\\opps\\"))
-    ).sort((a,b) =>
-      String(a.data?.deadline || "").localeCompare(String(b.data?.deadline || ""))
-    );
-  });
-
   return {
     dir: { input: "src", output: "_site" },
     markdownTemplateEngine: "njk",
