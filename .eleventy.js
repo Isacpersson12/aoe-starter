8ee16010cca6aebed9c4be0d68e68e01d82beefd
Isacpersson12 <ip.isacpersson@gmail.com>
2025-08-11 15:26:15 +0200

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,6 @@
 export default function(eleventyConfig) {
   eleventyConfig.addCollection("opps", (api) =>
-    api.getFilteredByGlob("opps/**/*.md")   // <-- key fix
+    api.getFilteredByGlob("opps/**/*.md") // because input dir is "src"
   );
   return {
     dir: { input: "src", output: "_site" },
